120be425a61ec9eb9f6f8290586fb51155604ca9
Aswin Gokulachandran <aswin.gokulachandran87@gmail.com>
2016-12-26 20:26:52 -0800

    optimus prime

---
+++ app/app.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Aswin on 6/4/2016.
+ */
+// Code goes here
+
+(function(){
+
+    angular
+        .module('myapp',['ngMessages','ngRoute'])
+        .config(myappConfigfn);
+
+    myappConfigfn.$inject = ['$routeProvider'];
+
+    function myappConfigfn($routeProvider){
+        $routeProvider
+            .when('/users',{
+                templateUrl: 'app/views/userlist-tmpl.html',
+                controller: 'UserListCtrl',
+                controllerAs: 'UserListVm'
+            })
+            .when('/users/:id',{
+                templateUrl: 'app/views/userdetails-tmpl.html',
+                controller: 'UserDetailsCtrl',
+                controllerAs: 'UserDetailsVm'
+            })
+            .when('/newuser',{
+                templateUrl: 'app/views/adduser-tmpl.html',
+                controller: 'AddUserCtrl',
+                controllerAs: 'addUserVm'
+            })
+            .otherwise({
+                redirectTo: '/users'
+            });
+    }
+
+
+
+})();
